15759f22c7c100026575c540f3cc796d725dee00
Willian Melnechuky <willianmeln@gmail.com>
2024-04-10 23:24:01 -0400

    Addressing minor bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,13 +11,15 @@ export default function Header() {
   const description = reactDescriptions[genRandomInt(2)];
 
   return (
+    <>
     <header>
       <img src={reactImg} alt="Logo" />
       <h1>Jane Hairstylist</h1>
-      {/* <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p> */}
     </header>
+    <p>
+      {description} React concepts you will need for almost any app you are
+      going to build!
+  </p>
+  </>
   );
 }
