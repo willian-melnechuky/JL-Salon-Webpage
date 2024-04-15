5e5206d096e88f85de227c9fb8ad838411dde977
Willian Melnechuky <willianmeln@gmail.com>
2024-04-14 22:04:56 -0400

    modification for upload 7

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import './Hero.css';
-import '../GallerySlideshow/GallerySlideshow.jsx'
+import image_1 from '../../assets/gallery/image_1.jpg';
 //import GallerySlideshow from '../GallerySlideshow/GallerySlideshow.jsx';
 
 export default function Hero() {
@@ -10,6 +10,7 @@ export default function Hero() {
           <h4>Refresh your elegance</h4>
           <p>Where beauty meets relaxation</p>
         </section>
+        <img className='hero__img' src={image_1} alt="" />
         {/* <GallerySlideshow/> */}
       </article>
     );
