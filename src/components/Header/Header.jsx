1505fb8ef9a6eefd407874288cf98ccb1f5f2e2f
Willian Melnechuky <willianmeln@gmail.com>
2024-04-14 22:13:16 -0400

    modification for upload 11

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,10 +8,10 @@ export default function Header({page}) {
       <h5><a>Jane Hairstylist</a></h5>
       {/* <img src={reactImg} alt="Logo" /> */}
       <ul className="nav-bar__items">
-        <li className={"nav-bar__items" + (page ==='Home'      ? '--selected' : "")}><a href=''>Home</a></li>
-        <li className={"nav-bar__items" + (page ==='Services'  ? '--selected' : "")}><a href=''>Services</a></li>
-        <li className={"nav-bar__items" + (page ==='Contact'   ? '--selected' : "")}><a href=''>Contact</a></li>
-        <li className={"nav-bar__items" + (page ==='Hours'     ? '--selected' : "")}><a href=''>Hours</a></li>
+        <li className={"nav-bar__items" + (page ==='Home'      ? '--selected' : "")}><a href='.nav-bar'>Home</a></li>
+        <li className={"nav-bar__items" + (page ==='Services'  ? '--selected' : "")}><a href='.nav-bar'>Services</a></li>
+        <li className={"nav-bar__items" + (page ==='Contact'   ? '--selected' : "")}><a href='.nav-bar'>Contact</a></li>
+        <li className={"nav-bar__items" + (page ==='Hours'     ? '--selected' : "")}><a href='.nav-bar'>Hours</a></li>
       </ul>
     </header>
   );
