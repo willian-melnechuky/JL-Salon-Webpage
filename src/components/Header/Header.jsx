c79181e812fe3b58e0a464db6a74e1cad5ac8683
Willian Melnechuky <willianmeln@gmail.com>
2024-05-31 18:01:18 -0400

    Adjusted Formatting to optimize phone view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,10 +9,11 @@ export default function Header({page}) {
       <h5><a href='.nav-bar'>Jane Hairstylist</a></h5>
       {/* <img src={reactImg} alt="Logo" /> */}
       <ul className="nav-bar__items">
-        <li className={"nav-bar__items" + (page ==='Home'      ? '--selected' : "")}><a href='index.html'>Home</a></li>
+        <li className={"nav-bar__items" + (page ==='Book Now'     ? '--selected' : "")}><a href='#hero' className='highlight'>Book Now</a></li>
+        {/* <li className={"nav-bar__items" + (page ==='Home'      ? '--selected' : "")}><a href='index.html'>Home</a></li> */}
         <li className={"nav-bar__items" + (page ==='Services'  ? '--selected' : "")}><a href='#services'>Services</a></li>
         <li className={"nav-bar__items" + (page ==='Hours'     ? '--selected' : "")}><a href='#hours'>Hours</a></li>
-        <li className={"nav-bar__items" + (page ==='Book Now'     ? '--selected' : "")}><a href='#hero' className='highlight'>Book Now</a></li>
+        
         {/* <li className={"nav-bar__items" + (page ==='Book Now'   ? '--selected' : "")}><Button size="small"/></li> */}
       </ul>
     </header>
