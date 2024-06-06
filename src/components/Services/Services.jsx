efd1356c599ea97f1fc10d095b2479543e744765
Willian Melnechuky <willianmeln@gmail.com>
2024-06-05 21:51:18 -0400

    Updated background Image and Prices formatting

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -10,9 +10,9 @@ export default function Services() {
         <article className="our-product" id ="services">
             <h3 className="our-product__h2">Our Services</h3>
             <div className="our-product__list">
-                <Card service="Female haircut" desc='Shampoo, haircut and basic hair drying. 10$ plus for hairstyle' price='$45' img_path={female_cut}/>
+                <Card service="Female haircut" desc='Shampoo, haircut and basic hair drying. $10 plus for hairstyle' price='$45' img_path={female_cut}/>
                 <Card service="Highlights" desc='Ask for more information' price='' img_path={hair_colour}/>
-                <Card service="Male haircut" desc='Shampoo and haircut. 10$ plus for beard trims' price='$25' img_path={male_cut}/>
+                <Card service="Male haircut" desc='Shampoo and haircut. $10 plus for beard trims' price='$25' img_path={male_cut}/>
                 <Card service="Nails" desc='Ask for more information' price='' img_path={nails}/>
             
             </div>
